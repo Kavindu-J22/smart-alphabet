3c3d7484c595655e4062a222e3d1dde9bc8bcfcf
Kavindu-J22 <it21032806@my.sliit.lk>
2023-03-16 08:44:13 +0530

    Updates

---
+++ precache-manifest.7140936c70079b02eae8d231950fcc09.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54539dd06df154b5a92a6bf7cdf065b",
+    "url": "/smart-alphabet/index.html"
+  },
+  {
+    "revision": "1b82b3c312f0ca0bfaa8",
+    "url": "/smart-alphabet/static/css/2.3ab4c7df.chunk.css"
+  },
+  {
+    "revision": "6f483252e101a475cc24",
+    "url": "/smart-alphabet/static/css/main.fc64a63b.chunk.css"
+  },
+  {
+    "revision": "1b82b3c312f0ca0bfaa8",
+    "url": "/smart-alphabet/static/js/2.76243e12.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/smart-alphabet/static/js/2.76243e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f483252e101a475cc24",
+    "url": "/smart-alphabet/static/js/main.a30bdd89.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/smart-alphabet/static/js/main.a30bdd89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3679b3d6bf5b49f47e0",
+    "url": "/smart-alphabet/static/js/runtime-main.2d016cd3.js"
+  },
+  {
+    "revision": "7b6dd9403c783c8a2d2c5b5f33b5d27b",
+    "url": "/smart-alphabet/static/media/AKlogo.7b6dd940.png"
+  },
+  {
+    "revision": "1ab001f5aaf09858f713c5500e1b78f4",
+    "url": "/smart-alphabet/static/media/Abt1.1ab001f5.png"
+  },
+  {
+    "revision": "69a4493f90390c97bbcdb7d2030e6af8",
+    "url": "/smart-alphabet/static/media/Abt2.69a4493f.png"
+  },
+  {
+    "revision": "33aae171fd080ba13e6dcb943a17e5b4",
+    "url": "/smart-alphabet/static/media/Abt3.33aae171.png"
+  },
+  {
+    "revision": "d9331125ede0100efbabf83b1d2b040d",
+    "url": "/smart-alphabet/static/media/Ayanna.d9331125.png"
+  },
+  {
+    "revision": "07b8c1ae0f06837c497ba99653d3cc20",
+    "url": "/smart-alphabet/static/media/Ayannawelcome.07b8c1ae.png"
+  },
+  {
+    "revision": "0e345818692350273300d940580d7695",
+    "url": "/smart-alphabet/static/media/B.0e345818.png"
+  },
+  {
+    "revision": "05f758cbe66b1cf1594676f749911528",
+    "url": "/smart-alphabet/static/media/SEphoto.05f758cb.png"
+  },
+  {
+    "revision": "40c519b97a7e9963091d383019a5aa9c",
+    "url": "/smart-alphabet/static/media/VranaLet.40c519b9.png"
+  },
+  {
+    "revision": "ae0e222dac269838b3e4f6e83a4bbe6f",
+    "url": "/smart-alphabet/static/media/s1.ae0e222d.png"
+  },
+  {
+    "revision": "5bca459341303f117de3c0d833eeae95",
+    "url": "/smart-alphabet/static/media/s2.5bca4593.png"
+  },
+  {
+    "revision": "5426ab4456d5afc73ffeead27d177a99",
+    "url": "/smart-alphabet/static/media/s3.5426ab44.png"
+  },
+  {
+    "revision": "0f796a62a8ff202c15d4b5ed806a52af",
+    "url": "/smart-alphabet/static/media/s4.0f796a62.png"
+  },
+  {
+    "revision": "1b8a4eb1a1645b89f01a5b4fd2bc831b",
+    "url": "/smart-alphabet/static/media/s5.1b8a4eb1.png"
+  },
+  {
+    "revision": "a5f4d84aa38786baad8dc06e8a4d44d3",
+    "url": "/smart-alphabet/static/media/s6.a5f4d84a.png"
+  },
+  {
+    "revision": "c4193b60dc69898c4f60ceae90f67890",
+    "url": "/smart-alphabet/static/media/ss1.c4193b60.png"
+  },
+  {
+    "revision": "a89623a666d0e33cc676bcb3bdbcbb45",
+    "url": "/smart-alphabet/static/media/video8.a89623a6.mp4"
+  }
+]);
